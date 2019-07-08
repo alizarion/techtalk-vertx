fe22270f53b9e0938f473373a6803ddee7b06949
Selim Bensenouci <selim.bensenouci@whoz.com>
2019-07-08 12:13:57 +0200

    s0 : deploy hello world kotlin

--- techtalk/backend/web/src/main/kotlin/io/github/alizarion/backend/web/MainVerticle.kt
+++ techtalk/backend/web/src/main/kotlin/io/github/alizarion/backend/web/MainVerticle.kt
@@ -2,11 +2,9 @@ package io.github.alizarion.backend.web
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
-import io.vertx.core.Vertx
+
 
 class MainVerticle : AbstractVerticle() {
-
-
 
 
     override fun start(startFuture: Future<Void>) {
--- techtalk/backend/web/src/main/kotlin/io/github/alizarion/backend/web/main.kt
+++ techtalk/backend/web/src/main/kotlin/io/github/alizarion/backend/web/main.kt
@@ -2,7 +2,7 @@ package io.github.alizarion.backend.web
 
 import io.vertx.core.Vertx
 
-fun main(args: Array<String>){
+fun main(){
     val vertx = Vertx.vertx()
     vertx.deployVerticle(MainVerticle::class.java.name)
 }
