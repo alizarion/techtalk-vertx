2f31ce0af2745317d04ac3b1e5fc1fd93863d7e3
Selim Bensenouci <selim.bensenouci@whoz.com>
2019-07-09 12:56:53 +0200

    S1 : hello world

---
+++ backend/web/src/main/kotlin/io/github/alizarion/backend/web/MainVerticle.kt
@@ -0,0 +1,19 @@
+package io.github.alizarion.backend.web
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+
+
+class MainVerticle : AbstractVerticle() {
+
+
+    override fun start(startFuture: Future<Void>) {
+        vertx.createHttpServer()
+                .requestHandler { req ->  req.response().end("Hello world!") }
+                .listen(8080)
+
+    }
+
+
+}
+
