2631662ee8faeb4a363916fadb57cd2372a0afff
Selim Bensenouci <selim.bensenouci@whoz.com>
2019-07-09 14:10:09 +0200

    S5 : deploy with fabric8

--- backend/web/src/main/kotlin/io/github/alizarion/backend/web/MainVerticle.kt
+++ backend/web/src/main/kotlin/io/github/alizarion/backend/web/MainVerticle.kt
@@ -7,13 +7,25 @@ import io.vertx.core.Future
 class MainVerticle : AbstractVerticle() {
 
 
-
     override fun start(startFuture: Future<Void>) {
-        vertx.createHttpServer()
-                .requestHandler { req ->  req.response().end("Hello world!") }
-                .listen(8080)
 
+        vertx.createHttpServer()
+                .requestHandler { req ->
+                    vertx.eventBus().send<Any>("hello-world-service", "yah !") { ar ->
+                        if(ar.succeeded()){
+
+                            val message = ar.result().body()
+                            req.response().end(message as String)
+
+                        } else {
+                            req.response().setStatusCode(500).end(ar.cause().toString())
+                        }
+                    }
+                }
+                .listen(8080)
     }
+
+
 
 }
 
