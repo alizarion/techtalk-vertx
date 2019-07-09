e7870b5e0eb2533e0408ed0ee16286345cbcf1f1
Selim Bensenouci <selim.bensenouci@whoz.com>
2019-07-09 13:08:13 +0200

    S3 : hello world + eventbus service

--- backend/services/src/main/kotlin/io/github/alizarion/backend/services/main.kt
+++
@@ -1,8 +0,0 @@
-package io.github.alizarion.backend.services
-
-import io.vertx.core.Vertx
-
-fun main(){
-    val vertx = Vertx.vertx()
-    vertx.deployVerticle(ServiceVerticle::class.java.name)
-}
